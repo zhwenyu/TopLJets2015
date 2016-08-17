ef1b0da9b443166c66769038343524389d7cfd3f
Pedro Silva <psilva@cern.ch>
2016-08-17 22:52:17 +0200

    move mlb map definition to outside the loop

--- TopAnalysis/scripts/steerTOPWidthAnalysis.sh
+++ TopAnalysis/scripts/steerTOPWidthAnalysis.sh
@@ -44,7 +44,7 @@ RED='\e[31m'
 NC='\e[0m'
 case $WHAT in
     SEL )
-	python scripts/runLocalAnalysis.py -i ${eosdir} -q ${queue} -o ${outdir} --era ${ERA} -m TOP-16-019::RunTop16019 --ch 0 --only 50toInf_madgraph;
+	python scripts/runLocalAnalysis.py -i ${eosdir} -q ${queue} -o ${outdir} --era ${ERA} -m TOP-16-019::RunTop16019 --ch 0;
 	;;
     MERGESEL )
 	./scripts/mergeOutputs.py ${outdir} True;	
@@ -58,7 +58,8 @@ case $WHAT in
 	cp test/index.php ${wwwdir}/sel
 	;;
     ANA )
-	python scripts/runTopWidthAnalysis.py -i ${summaryeosdir} -o ${outdir}/analysis -q ${queue};
+	queue=local
+	python scripts/runTopWidthAnalysis.py -i ${summaryeosdir} -o ${outdir}/analysis -q ${queue} --only TTJ;
 	;;
     MERGE )
 	./scripts/mergeOutputs.py ${outdir}/analysis;
