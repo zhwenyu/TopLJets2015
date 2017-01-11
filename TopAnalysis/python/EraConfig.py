7ae5636857b0d59d4105e4bf32164c19758133c1
Pedro Silva <psilva@cern.ch>
2017-01-11 13:25:28 +0100

    fixing muon correction link

--- TopAnalysis/python/EraConfig.py
+++ TopAnalysis/python/EraConfig.py
@@ -3,7 +3,7 @@ import os
 def getEraConfiguration(era,isData):
     globalTags = {'era2016':('80X_mcRun2_asymptotic_2016_miniAODv2_v1', '80X_dataRun2_2016SeptRepro_v5')  }
     jecTags    = {'era2016':('Spring16_23Sep2016V2',                    'Spring16_23Sep2016AllV2')}
-    muonCorrs  = {'era2016':('RoccoR_13tev.txt',                        'RoccoR_13tev.txt')}
+    muonCorrs  = {'era2016':('RoccoR_13tev',                            'RoccoR_13tev')}
 
     idx=1 if isData else 0
     globalTag = globalTags[era][idx]
@@ -12,9 +12,11 @@ def getEraConfiguration(era,isData):
     jecDB  = 'jec_DATA.db'  if isData else 'jec_MC.db'
     muonDB = 'muon_DATA.txt' if isData else 'muon_MC.txt'
     if not os.path.isfile(jecDB) and not os.path.islink(jecDB):
-        print 'Creating symbolic link to correction dbs'
+        print 'Creating symbolic link to DB correction DB'
         os.system('ln -s ${CMSSW_BASE}/src/TopLJets2015/TopAnalysis/data/%s/%s_%s.db %s'%(era,jecTag,jecTag_pf,jecDB))
-        os.system('ln -s ${CMSSW_BASE}/src/TopLJets2015/TopAnalysis/data/%s/%s.db %s'%(era,muonCorrs[era][isData],muonDB))
+    if not os.path.isfile(muonDB) and not os.path.islink(muonDB):
+        print 'Creating symbolic link to muon correction txt file'
+        os.system('ln -s ${CMSSW_BASE}/src/TopLJets2015/TopAnalysis/data/%s/%s.txt %s'%(era,muonCorrs[era][isData],muonDB))
 
     return globalTag, jecTag, jecDB, muonDB
     
