e1b9b68e42c55815d40f15e9b9de73d6a89e0a95
Pedro Silva <psilva@cern.ch>
2017-02-10 13:06:14 +0100

    adding new RoccoR corresctions

--- TopAnalysis/src/RoccoR.cc
+++ TopAnalysis/src/RoccoR.cc
@@ -1,14 +1,26 @@
-#include "TRandom3.h"
+#ifndef ElectroWeakAnalysis_RoccoR
+#define ElectroWeakAnalysis_RoccoR
+
+#include <fstream>
+#include <sstream>
+#include "TSystem.h"
 #include "TMath.h"
-#include "TopLJets2015/TopAnalysis/interface/RoccoR.h"
+#include "RoccoR.h"
 
 
-int RocRes::getBin(double x, const int NN, const double *b){
+int RocRes::getBin(double x, const int NN, const double *b) const{
     for(int i=0; i<NN; ++i) if(x<b[i+1]) return i;
     return NN-1;
 }
 
-RocRes::RocRes():NETA(1),NTRK(1),NMIN(1){
+RocRes::RocRes(){
+    reset();
+}
+
+void RocRes::reset(){
+    NETA=1;
+    NTRK=1;
+    NMIN=1;
     for(int H=0; H<NMAXETA; ++H){
 	BETA[H]=0;
 	kDat[H]=1.0;
@@ -27,19 +39,21 @@ RocRes::RocRes():NETA(1),NTRK(1),NMIN(1){
     BETA[NMAXETA]=0;
 }
 
-int RocRes::getEtaBin(double feta){
+int RocRes::getEtaBin(double feta) const{
     return getBin(feta,NETA,BETA);
 }
 
-int RocRes::getNBinDT(double v, int H){
+int RocRes::getNBinDT(double v, int H) const{
     return getBin(v,NTRK,dtrk[H]);
 }
 
-int RocRes::getNBinMC(double v, int H){
+int RocRes::getNBinMC(double v, int H) const{
     return getBin(v,NTRK,ntrk[H]);
 }
 
 void RocRes::dumpParams(){
+    using namespace std;
+
     cout << NMIN << endl;
     cout << NTRK << endl;
     cout << NETA << endl;
@@ -68,63 +82,66 @@ void RocRes::dumpParams(){
 
 
 	
-void RocRes::init(string filename){
-  std::ifstream in(filename);
-  char buffer[256];
-  char tag[4];
-  int type, sys, mem, isdt, var, bin;	
-  std::string s;
-  while(std::getline(in, s)){
-    stringstream ss(s); 
-    if(s.substr(0,4)=="RMIN")       ss >> tag >> NMIN;
-    else if(s.substr(0,4)=="RTRK")  ss >> tag >> NTRK;
-    else if(s.substr(0,4)=="RETA")  {
-      ss >> tag >> NETA;
-      for(int i=0; i< NETA+1; ++i) ss >> BETA[i];
-    }
-    else if(s.substr(0,1)=="R")  {
-      ss >> tag >> type >> sys >> mem >> isdt >> var >> bin; 
-      if(var==0) for(int i=0; i<NTRK; ++i) ss >> rmsA[bin][i];  
-      if(var==1) for(int i=0; i<NTRK; ++i) ss >> rmsB[bin][i];  
-      if(var==2) for(int i=0; i<NTRK; ++i) {
-	  ss >> rmsC[bin][i];  
-	  rmsC[bin][i]/=100;
+void RocRes::init(std::string filename){
+    std::ifstream in(filename.c_str());
+    char tag[4];
+    int type, sys, mem, isdt, var, bin;	
+    std::string s;
+    while(std::getline(in, s)){
+	std::stringstream ss(s); 
+	if(s.substr(0,4)=="RMIN")       ss >> tag >> NMIN;
+	else if(s.substr(0,4)=="RTRK")  ss >> tag >> NTRK;
+	else if(s.substr(0,4)=="RETA")  {
+	    ss >> tag >> NETA;
+	    for(int i=0; i< NETA+1; ++i) ss >> BETA[i];
 	}
-      if(var==3) for(int i=0; i<NTRK; ++i) ss >> width[bin][i];  
-      if(var==4) for(int i=0; i<NTRK; ++i) ss >> alpha[bin][i];  
-      if(var==5) for(int i=0; i<NTRK; ++i) ss >> power[bin][i];  
+	else if(s.substr(0,1)=="R")  {
+	    ss >> tag >> type >> sys >> mem >> isdt >> var >> bin; 
+	    if(var==0) for(int i=0; i<NTRK; ++i) ss >> rmsA[bin][i];  
+	    if(var==1) for(int i=0; i<NTRK; ++i) ss >> rmsB[bin][i];  
+	    if(var==2) for(int i=0; i<NTRK; ++i) {
+		ss >> rmsC[bin][i];  
+		rmsC[bin][i]/=100;
+	    }
+	    if(var==3) for(int i=0; i<NTRK; ++i) ss >> width[bin][i];  
+	    if(var==4) for(int i=0; i<NTRK; ++i) ss >> alpha[bin][i];  
+	    if(var==5) for(int i=0; i<NTRK; ++i) ss >> power[bin][i];  
+	}
+	else if(s.substr(0,1)=="T")  {
+	    ss >> tag >> type >> sys >> mem >> isdt >> var >> bin; 
+	    if(isdt==0) for(int i=0; i<NTRK+1; ++i) ss >> ntrk[bin][i];  
+	    if(isdt==1) for(int i=0; i<NTRK+1; ++i) ss >> dtrk[bin][i];  
+	}
+	else if(s.substr(0,1)=="F")  {
+	    ss >> tag >> type >> sys >> mem >> isdt >> var >> bin; 
+	    if(var==0){
+		if(isdt==0) for(int i=0; i<NETA; ++i) ss >> kRes[i];  
+		if(isdt==1) for(int i=0; i<NETA; ++i) ss >> kDat[i];  
+	    }
+	}
     }
-    else if(s.substr(0,1)=="T")  {
-      ss >> tag >> type >> sys >> mem >> isdt >> var >> bin; 
-      if(isdt==0) for(int i=0; i<NTRK+1; ++i) ss >> ntrk[bin][i];  
-      if(isdt==1) for(int i=0; i<NTRK+1; ++i) ss >> dtrk[bin][i];  
-    }
-    else if(s.substr(0,1)=="F")  {
-      ss >> tag >> type >> sys >> mem >> isdt >> var >> bin; 
-      if(var==0){
-	if(isdt==0) for(int i=0; i<NETA; ++i) ss >> kRes[i];  
-	if(isdt==1) for(int i=0; i<NETA; ++i) ss >> kDat[i];
-      }
-    }
-  }
 
-  for(int H=0; H<NETA; ++H){
-    for(int F=0; F<NTRK; ++F){
-      cb[H][F].init(0.0, width[H][F], alpha[H][F], power[H][F]);
+    for(int H=0; H<NETA; ++H){
+	for(int F=0; F<NTRK; ++F){
+	    cb[H][F].init(0.0, width[H][F], alpha[H][F], power[H][F]);
+	}
     }
-  }
-  in.close();
+    in.close();
 }
 
-double RocRes::Sigma(double pt, int H, int F){
+double RocRes::Sigma(double pt, int H, int F) const{
     double dpt=pt-45;
     return rmsA[H][F] + rmsB[H][F]*dpt + rmsC[H][F]*dpt*dpt;
 }
 
-double RocRes::kSpreadDet(double gpt, double rpt, double eta, int nlayers, double w){
+double RocRes::getUrnd(int H, int F, double w) const{
+    return ntrk[H][F]+(ntrk[H][F+1]-ntrk[H][F])*w; 
+}
+
+double RocRes::kSpread(double gpt, double rpt, double eta, int n, double w) const{
     int     H = getBin(fabs(eta), NETA, BETA);
-    int     F = nlayers-NMIN;
-    double  v = ntrk[H][F]+(ntrk[H][F+1]-ntrk[H][F])*w;
+    int     F = n>NMIN ? n-NMIN : 0;
+    double  v = getUrnd(H, F, w);
     int     D = getBin(v, NTRK, dtrk[H]);
     double  kold = gpt / rpt;
     double  u = cb[H][F].cdf( (kold-1.0)/kRes[H]/Sigma(gpt,H,F) ); 
@@ -133,78 +150,57 @@ double RocRes::kSpreadDet(double gpt, double rpt, double eta, int nlayers, doubl
     return kold/knew;
 }
 
-double RocRes::kSmearDet(double pt, double eta, TYPE type, double v, double u){
+double RocRes::kSmear(double pt, double eta, TYPE type, double v, double u) const{
     int H = getBin(fabs(eta), NETA, BETA);
-    const double (&trk) [NMAXTRK+1] = type==Data ? dtrk[H] : ntrk[H];
-    int F = getBin(v, NTRK, trk);
+    int F = type==Data? getNBinDT(v, H) : getNBinMC(v, H);
     double K = type==Data ? kDat[H] : kRes[H]; 
     double x = K*Sigma(pt, H, F)*cb[H][F].invcdf(u);
     return 1.0/(1.0+x);
 }
 
-double RocRes::kExtraDet(double pt, double eta, int nlayers, double u, double w){
+double RocRes::kSmear(double pt, double eta, TYPE type, double w, double u, int n) const{
     int H = getBin(fabs(eta), NETA, BETA);
-    int F = nlayers-NMIN;
+    int F = n>NMIN ? n-NMIN : 0;
+    if(type==Data) F = getNBinDT(getUrnd(H, F, w), H);
+    double K = type==Data ? kDat[H] : kRes[H]; 
+    double x = K*Sigma(pt, H, F)*cb[H][F].invcdf(u);
+    return 1.0/(1.0+x);
+}
+
+double RocRes::kExtra(double pt, double eta, int n, double u, double w) const{
+    int H = getBin(fabs(eta), NETA, BETA);
+    int F = n>NMIN ? n-NMIN : 0;
     double  v = ntrk[H][F]+(ntrk[H][F+1]-ntrk[H][F])*w;
     int     D = getBin(v, NTRK, dtrk[H]);
     double RD = kDat[H]*Sigma(pt, H, D);
     double RM = kRes[H]*Sigma(pt, H, F);
-    double x = RD>RM ? sqrt(RD*RD-RM*RM)*cb[H][F].invcdf(u) : 0;
+    if(RD<=RM) return 1.0; 
+    double r=cb[H][F].invcdf(u);
+    if(fabs(r)>5) return 1.0; //protection against too large smearing
+    double x = sqrt(RD*RD-RM*RM)*r;
     if(x<=-1) return 1.0;
     return 1.0/(1.0 + x); 
 }
 
-void RocRes::fillFitData(int &H, int &F, int &D, double &xmc, double &xdt, double &Rmc, double &Rdt, double pt, double eta){
-    H = getBin(fabs(eta), NETA, BETA);
-    double u=random.Rndm();
-    F = getBin(u, NTRK, ntrk[H]); 
-    D = getBin(u, NTRK, dtrk[H]);
-    Rmc = Sigma(pt, H, F); 
-    Rdt = Sigma(pt, H, D); 
-    double v=random.Rndm();
-    xmc = Rmc*cb[H][F].invcdf(v); 
-    xdt = Rdt*cb[H][D].invcdf(v); 
-    return;
-}
 
-
-
-
-
-
-
-
-
-
+//-------------------------------------
 
 const double RocOne::MPHI=-TMath::Pi();
 
-int RocOne::getBin(double x, const int NN, const double *b){
+int RocOne::getBin(double x, const int NN, const double *b) const{
     for(int i=0; i<NN; ++i) if(x<b[i+1]) return i;
     return NN-1;
 }
 
-int RocOne::getBin(double x, const int nmax, const double xmin, const double dx){
+int RocOne::getBin(double x, const int nmax, const double xmin, const double dx) const{
     int ibin=(x-xmin)/dx;
     if(ibin<0) return 0; 
     if(ibin>=nmax) return nmax-1;
     return ibin;
 }
 
-RocOne::RocOne():NETA(1),NPHI(1){
-    DPHI=2*TMath::Pi()/NPHI;
-    for(int H=0; H<NMAXETA; ++H){
-	BETA[H]=0;
-	D[MC][H]=1.0;
-	D[DT][H]=1.0;
-	for(int F=0; F<NMAXPHI; ++F){
-	    for(int T=0; T<2; ++T){
-		M[T][H][F]=1;
-		A[T][H][F]=0;
-	    }
-	}
-    }
-    BETA[NMAXETA]=0;
+RocOne::RocOne(){
+    reset();
 }
 
 RocOne::RocOne(std::string filename, int iTYPE, int iSYS, int iMEM){
@@ -225,17 +221,41 @@ bool RocOne::checkTIGHT(int iTYPE, int iSYS, int iMEM, int kTYPE, int kSYS, int 
     return true;
 }
 
+void RocOne::reset(){
+    RR.reset();
+
+    NETA=1;
+    NPHI=1;
+    DPHI=2*TMath::Pi()/NPHI;
+    for(int H=0; H<NMAXETA; ++H){
+	BETA[H]=0;
+	D[MC][H]=1.0;
+	D[DT][H]=1.0;
+	for(int F=0; F<NMAXPHI; ++F){
+	    for(int T=0; T<2; ++T){
+		M[T][H][F]=1;
+		A[T][H][F]=0;
+	    }
+	}
+    }
+    BETA[NMAXETA]=0;
+}
+
 void RocOne::init(std::string filename, int iTYPE, int iSYS, int iMEM){
+
+    reset();
 
     RR.init(filename);
 
-    std::ifstream in(filename);
+    std::ifstream in(filename.c_str());
     char tag[4];
     int type, sys, mem, isdt, var, bin;	
 
+    bool initialized=false;
+
     std::string s;
     while(std::getline(in, s)){
-	stringstream ss(s); 
+	std::stringstream ss(s); 
 	if(s.substr(0,4)=="CPHI")       {
 	    ss >> tag >> NPHI;
 	    DPHI=2*TMath::Pi()/NPHI;
@@ -247,6 +267,7 @@ void RocOne::init(std::string filename, int iTYPE, int iSYS, int iMEM){
 	else if(s.substr(0,1)=="C")  {
 	    ss >> tag >> type >> sys >> mem >> isdt >> var >> bin; 
 	    if(!checkTIGHT(type,sys,mem,iTYPE,iSYS,iMEM)) continue;
+	    initialized=true;
 	    if(var==0) for(int i=0; i<NPHI; ++i) { ss >> M[isdt][bin][i]; M[isdt][bin][i]/=100; M[isdt][bin][i]+=1.0;} 
 	    if(var==1) for(int i=0; i<NPHI; ++i) { ss >> A[isdt][bin][i]; A[isdt][bin][i]/=100;} 
 
@@ -262,21 +283,23 @@ void RocOne::init(std::string filename, int iTYPE, int iSYS, int iMEM){
 	    }
 	}
     }
+    if(!initialized) std::cout << "Problem with input file: " << filename << std::endl;
     in.close();
 }
 
-double RocOne::kScaleDT(int Q, double pt, double eta, double phi){
+double RocOne::kScaleDT(int Q, double pt, double eta, double phi) const{
     int H=getBin(eta, NETA, BETA);
     int F=getBin(phi, NPHI, MPHI, DPHI);
     double m=M[DT][H][F];
     double a=A[DT][H][F];
     double d=D[DT][H];
+
     double k=d/(m+Q*a*pt);
     return k;
 }
 
 
-double RocOne::kScaleMC(int Q, double pt, double eta, double phi, double kSMR){
+double RocOne::kScaleMC(int Q, double pt, double eta, double phi, double kSMR) const{
     int H=getBin(eta, NETA, BETA);
     int F=getBin(phi, NPHI, MPHI, DPHI);
     double m=M[MC][H][F];
@@ -286,176 +309,89 @@ double RocOne::kScaleMC(int Q, double pt, double eta, double phi, double kSMR){
     return k*kSMR;
 }
 
-double RocOne::kScaleAndSmearMCDet(int Q, double pt, double eta, double phi, int nlayers, double u, double w){
+double RocOne::kScaleAndSmearMC(int Q, double pt, double eta, double phi, int n, double u, double w) const{
     double k=kScaleMC(Q, pt, eta, phi);
-    return k*RR.kExtraDet(k*pt, eta, nlayers, u, w);
+    return k*RR.kExtra(k*pt, eta, n, u, w);
 }
 
 
-double RocOne::kScaleFromGenMCDet(int Q, double pt, double eta, double phi, double gpt, int nlayers, double w){
+double RocOne::kScaleFromGenMC(int Q, double pt, double eta, double phi, int n, double gt, double w) const{
     double k=kScaleMC(Q, pt, eta, phi);
-    return k*RR.kSpreadDet(gpt, k*pt, eta, nlayers, w);
+    return k*RR.kSpread(gt, k*pt, eta, n, w);
 }
 
 
-double RocOne::kGenSmearDet(double pt, double eta, double v, double u){
-    return RR.kSmearDet(pt, eta, RocRes::Data, v, u);
+double RocOne::kGenSmear(double pt, double eta, double v, double u, RocRes::TYPE TT) const{
+    return RR.kSmear(pt, eta, TT, v, u);
 }
 
 
+//-------------------------------------
 
 
-
-
-
-
-const int RoccoR::Nmem[RoccoR::Nset]={1, 100, 1, 41, 21};
-
-RoccoR::RoccoR(){
-    for(int s=0; s<Nset; ++s){
-	for(int m=0; m<Nmem[s]; ++m){
-	    RC[s][m]=0;
-	}
-    }
-}
-
+RoccoR::RoccoR(){}
 
 RoccoR::RoccoR(std::string dirname){
-    for(int s=0; s<Nset; ++s){
-	for(int m=0; m<Nmem[s]; ++m){
-	    std::string inputfile=Form("%s/rc_%d_%d.txt", dirname.c_str(), s, m);
-	    RC[s][m]=new RocOne(inputfile, 0, s, m);
+    init(dirname);
+}
+
+
+void 
+RoccoR::init(std::string dirname){
+
+    std::string filename=Form("%s/config.txt", dirname.c_str());
+
+    std::ifstream in(filename.c_str());
+    std::string s;
+    std::string tag;
+    int si;
+    int sn;
+    while(std::getline(in, s)){
+	std::stringstream ss(s); 
+	ss >> tag >> si >> sn; 
+	std::vector<RocOne> v;
+	for(int m=0; m<sn; ++m){
+	    std::string inputfile=Form("%s/%d.%d.txt", dirname.c_str(), si, m);
+	    if(gSystem->AccessPathName(inputfile.c_str())) {
+		std::cout << Form("Missing %8d %3d, using default instead...", si, m) << std::endl;  
+		v.push_back(RocOne(Form("%s/%d.%d.txt", dirname.c_str(),0,0),0,0,0));
+	    }
+	    else{
+		v.push_back(RocOne(inputfile, 0, si, m));
+	    }
 	}
+	RC.push_back(v);
     }
+
+    in.close();
 }
 
-RoccoR::~RoccoR(){
-    for(int s=0; s<Nset; ++s){
-	for(int m=0; m<Nmem[s]; ++m){
-	    if(RC[s][m]) delete RC[s][m];
-	}
-    }
+RoccoR::~RoccoR(){}
+
+
+
+double RoccoR::kGenSmear(double pt, double eta, double v, double u, RocRes::TYPE TT, int s, int m) const{
+    return RC[s][m].kGenSmear(pt, eta, v, u, TT);
 }
 
-
-	
-double RoccoR::kScaleDT(int Q, double pt, double eta, double phi, TYPE T, int m){
-    return RC[T][m]->kScaleDT(Q, pt, eta, phi);
+double RoccoR::kScaleDT(int Q, double pt, double eta, double phi, int s, int m) const{
+    return RC[s][m].kScaleDT(Q, pt, eta, phi);
 }
 
-
-double RoccoR::kScaleAndSmearMCDet(int Q, double pt, double eta, double phi, int nlayers){
-    return RC[Default][0]->kScaleAndSmearMCDet(Q, pt, eta, phi, nlayers, random.Rndm(), random.Rndm());
-
-}
-	
-double RoccoR::kScaleFromGenMCDet(int Q, double pt, double eta, double phi, double gpt, int nlayers){
-    return RC[Default][0]->kScaleFromGenMCDet(Q, pt, eta, phi, gpt, nlayers, random.Rndm());
+double RoccoR::kScaleAndSmearMC(int Q, double pt, double eta, double phi, int n, double u, double w, int s, int m) const{
+    return RC[s][m].kScaleAndSmearMC(Q, pt, eta, phi, n, u, w);
 }
 
-double RoccoR::kScaleAndSmearMCDet(int Q, double pt, double eta, double phi, int nlayers, double u, double w, TYPE T, int m){
-    return RC[T][m]->kScaleAndSmearMCDet(Q, pt, eta, phi, nlayers, u, w);
-}
-
-double RoccoR::kScaleFromGenMCDet(int Q, double pt, double eta, double phi, double gpt, int nlayers, double w, TYPE T, int m){
-    return RC[T][m]->kScaleFromGenMCDet(Q, pt, eta, phi, gpt, nlayers, w);
-}
-
-std::vector<std::vector<double> > RoccoR::kkScaleAndSmearMCDet(int Q, double pt, double eta, double phi, int nlayers){
-
-    double u=random.Rndm();
-    double w=random.Rndm();
-
-    std::vector<std::vector<double> > result;
-    for(int s=0; s<Nset; ++s){
-	std::vector<double> d;
-	for(int m=0; m<Nmem[s]; ++m){
-	    double k=kScaleAndSmearMCDet(Q, pt, eta, phi, nlayers, u,  w, TYPE(s), m);
-	    d.push_back(k);
-	}
-	result.push_back(d);
-    }
-    return result;
-}
-
-std::vector<std::vector<double> > RoccoR::kkScaleFromGenMCDet(int Q, double pt, double eta, double phi, double gpt, int nlayers){
-    double u=random.Rndm();
-    double w=random.Rndm();
-
-    std::vector<std::vector<double> > result;
-    for(int s=0; s<Nset; ++s){
-	std::vector<double> d;
-	for(int m=0; m<Nmem[s]; ++m){
-	    double k=kScaleFromGenMCDet(Q, pt, eta, phi, gpt, nlayers, w, TYPE(s),  m);
-	    d.push_back(k);
-	}
-	result.push_back(d);
-    }
-    return result;
+double RoccoR::kScaleFromGenMC(int Q, double pt, double eta, double phi, int n, double gt, double w, int s, int m) const{
+    return RC[s][m].kScaleFromGenMC(Q, pt, eta, phi, n, gt, w);
 }
 
 
+#endif
 
 
-CrystalBall::CrystalBall(){
-  init(0, 1, 10, 10);
-}
-CrystalBall::CrystalBall(double m_, double s_, double a_, double n_){
-  init(m_, s_, a_, n_);
-}
 
-void CrystalBall::init(double m_, double s_, double a_, double n_){
 
-  pi    = TMath::Pi();
-  SPiO2 = sqrt(TMath::Pi()/2.0);
-  S2    = sqrt(2.0);
 
-  m=m_;
-  s=s_;
-  a=a_;
-  n=n_;
-  
-  double fa   = fabs(a);
-  double expa = exp(-fa*fa/2);
-  double A    = pow(n/fa, n)*expa;
-  double C1   = n/fa/(n-1)*expa; 
-  double D1   = 2*SPiO2*erf(fa/S2);
-  
-  B  = n/fa-fa;
-  C  = (D1+2*C1)/C1;   
-  D  = (D1+2*C1)/2;   
-  
-  N  = 1.0/s/(D1+2*C1); 
-  k  = 1.0/(n-1);  
-  
-  NA = N*A;       
-  Ns = N*s;       
-  NC = Ns*C1;     
-  F  = 1-fa*fa/n; 
-  G  = s*n/fa;    
-  
-  cdfMa=cdf(m-a*s);
-  cdfPa=cdf(m+a*s);
-}
-
-double CrystalBall::pdf(double x){ 
-  double d=(x-m)/s;
-  if(d<-a) return NA*pow(B-d, -n);
-  if(d> a) return NA*pow(B+d, -n);
-  return N*exp(-d*d/2);
-}
-
-double CrystalBall::cdf(double x){
-  double d = (x-m)/s;
-  if(d<-a) return NC / pow(F-s*d/G, n-1);
-  if(d> a) return NC * (C - pow(F+s*d/G, 1-n) );
-  return Ns*(D-SPiO2*erf(-d/S2));
-}
-
-double CrystalBall::invcdf(double u){
-  if(u<cdfMa) return m + G*(F - pow(NC/u,    k) );
-  if(u>cdfPa) return m - G*(F - pow(C-u/NC, -k) );
-  return m - S2*s*TMath::ErfInverse((D - u/Ns ) / SPiO2);
-}
 
 
