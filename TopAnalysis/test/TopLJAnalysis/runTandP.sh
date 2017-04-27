3da4425449077965eb8b5a96c12337dfceb3fb84
Pedro Silva <psilva@cern.ch>
2017-04-27 23:23:01 +0200

    updating

--- TopAnalysis/test/TopLJAnalysis/runTandP.sh
+++ TopAnalysis/test/TopLJAnalysis/runTandP.sh
@@ -1,16 +1,19 @@
 f=(BWResCBExp) #CB2Exp)
 d=(True False)
-for i in ${f[@]}; do 
-    for j in ${d[@]}; do                        
-        continue
-        cmsRun runTandP_HLT.py data=${j} func=${i} & 
-	cmsRun runTandP_IDISO.py data=${j} func=${i} &  
-	cmsRun runTandP_HLTIDISO.py data=${j} func=${i} & 
-    done 
 
-    python drawTandPResults.py hltfits_pPb_8.16TeV_1_BWResCBExp.root:"Data",hltfits_pPb_8.16TeV_0_BWResCBExp.root:"MC" ~/www/LJets-pPb/HLT_mu
-    python drawTandPResults.py idisofits_pPb_8.16TeV_1_BWResCBExp.root:"Data",idisofits_pPb_8.16TeV_0_BWResCBExp.root:"MC" ~/www/LJets-pPb/IDISO_mu
-    python drawTandPResults.py hltidisofits_pPb_8.16TeV_1_BWResCBExp.root:"Data",hltidisofits_pPb_8.16TeV_0_BWResCBExp.root:"MC" ~/www/LJets-pPb/HLTIDISO0.4_mu
+for coll in Pbp; do #pPb
 
+    for i in ${f[@]}; do 
+        for j in ${d[@]}; do                        
+            continue
+            cmsRun runTandP_HLT.py      data=${j} func=${i} tree=${coll}_8.16TeV & 
+	    cmsRun runTandP_IDISO.py    data=${j} func=${i} tree=${coll}_8.16TeV &  
+	    cmsRun runTandP_HLTIDISO.py data=${j} func=${i} tree=${coll}_8.16TeV & 
+        done 
+
+    python drawTandPResults.py hltfits_${coll}_8.16TeV_1_BWResCBExp.root:"Data",hltfits_${coll}_8.16TeV_0_BWResCBExp.root:"MC"           ~/www/LJets-${coll}/HLT_mu
+    python drawTandPResults.py idisofits_${coll}_8.16TeV_1_BWResCBExp.root:"Data",idisofits_${coll}_8.16TeV_0_BWResCBExp.root:"MC"       ~/www/LJets-${coll}/IDISO_mu
+    python drawTandPResults.py hltidisofits_${coll}_8.16TeV_1_BWResCBExp.root:"Data",hltidisofits_${coll}_8.16TeV_0_BWResCBExp.root:"MC" ~/www/LJets-${coll}/HLTIDISO_mu
+    done
 done
 
