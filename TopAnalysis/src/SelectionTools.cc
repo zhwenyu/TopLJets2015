411dd48f20c434a9daca78754b503f0acf09d084
Markus Seidel <markus.seidel@cern.ch>
2017-07-20 16:27:06 +0200

    Require tight electron ID

--- TopAnalysis/src/SelectionTools.cc
+++ TopAnalysis/src/SelectionTools.cc
@@ -36,7 +36,7 @@ std::vector<Particle> SelectionTool::getTopFlaggedLeptons(MiniEvent_t &ev){
     if(abs(ev.l_id[il])==11)
       {
 	if( pt>20 && eta<2.4 && ((pid>>7) &0x1))                                     topLeptonQualityFlagsWord |= (0x1 << PASSLLID);
-	if( pt>34 && eta<2.1 && ((pid>>4) &0x1))                                     topLeptonQualityFlagsWord |= (0x1 << PASSLID);
+	if( pt>34 && eta<2.1 && ((pid>>7) &0x1))                                     topLeptonQualityFlagsWord |= (0x1 << PASSLID);
 	if( pt>15 && eta<2.4 && ((pid>>2) &0x1))                                     topLeptonQualityFlagsWord |= (0x1 << PASSLVETO);
 	if( pt>26 && eta<2.1 && ((pid>>5) &0x1) && ((pid>>4) &0x1)==0 && relIso>0.4) topLeptonQualityFlagsWord |= (0x1 << PASSLIDNONISO);
       }
