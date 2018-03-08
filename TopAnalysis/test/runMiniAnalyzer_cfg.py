bd4c6b1379483446e22bbf1df36a69dffdb09eb4
Pedro <psilva@cern.ch>
2018-03-08 17:09:45 +0100

    ready to run on data

--- TopAnalysis/test/runMiniAnalyzer_cfg.py
+++ TopAnalysis/test/runMiniAnalyzer_cfg.py
@@ -91,7 +91,8 @@ process.source = cms.Source("PoolSource",
                             )
 
 if options.runOnData:
-      process.source.fileNames = cms.untracked.vstring('/store/data/Run2017F/SingleElectron/MINIAOD/17Nov2017-v1/50000/FA333057-D0E0-E711-BDFB-5065F37DC062.root')
+      process.source.fileNames = cms.untracked.vstring('/store/data/Run2017B/SinglePhoton/MINIAOD/17Nov2017-v1/30000/10F66FEF-B7D5-E711-9003-008CFAC93F3C.root')
+      #process.source.fileNames = cms.untracked.vstring('/store/data/Run2017F/SingleElectron/MINIAOD/17Nov2017-v1/50000/FA333057-D0E0-E711-BDFB-5065F37DC062.root')
       #process.source.fileNames = cms.untracked.vstring('/store/data/Run2017F/SingleMuon/MINIAOD/17Nov2017-v1/50000/FEED7A3F-D3E2-E711-84DD-0025905A6134.root')
 
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
