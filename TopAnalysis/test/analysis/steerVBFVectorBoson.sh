f40657b14e8c19cc783268e848333998c97b11e5
Pedro <psilva@cern.ch>
2018-05-23 02:13:30 +0200

    uniformizing with 2018 dataset

--- TopAnalysis/test/analysis/steerVBFVectorBoson.sh
+++ TopAnalysis/test/analysis/steerVBFVectorBoson.sh
@@ -15,7 +15,8 @@ fi
 queue=workday
 githash=fbc74ae
 eosdir=/store/cmst3/group/top/RunIIFall17/${githash}
-eosdir2018=/store/cmst3/group/top/RunIISpring18/4ad3a45
+githash2018=4ad3a45
+eosdir2018=/store/cmst3/group/top/RunIISpring18/${githash2018}
 fulllumi=41367
 vbflumi=7661
 fulllumi2018=2300
@@ -60,7 +61,7 @@ case $WHAT in
 
     SEL2018 )
 	python scripts/runLocalAnalysis.py -i ${eosdir2018} \
-            -o ${outdir}/raw2018 \
+            -o ${outdir}/${githash2018}/raw2018 \
             -q ${queue} \
             --era era2017 -m VBFVectorBoson::RunVBFVectorBoson --ch 0 --runSysts;
 	;;
@@ -73,22 +74,28 @@ case $WHAT in
         ;;
 
     MERGE )
-	./scripts/mergeOutputs.py ${outdir}/${githash}/${EXTRA};
+        gh=${githash}
+        if [[ "${EXTRA}" = *"2018"* ]]; then
+            gh=${githash2018}
+        fi
+	./scripts/mergeOutputs.py ${outdir}/${gh}/${EXTRA};
 	;;
 
     PLOT )
         json=data/era2017/vbf_samples.json;
         lumi=${fulllumi}        
+        gh=${githash}
         if [[ "${EXTRA}" = *"2018"* ]]; then
             json=data/era2018/vbf_samples.json;
             lumi=${fulllumi2018}
             vbflumi=${lumi}
+            gh=${githash2018}
         fi
         kFactors="--procSF MC13TeV_QCDEM_15to20:1.26,MC13TeV_QCDEM_20to30:1.26,MC13TeV_QCDEM_30to50:1.26,MC13TeV_QCDEM_50to80:1.26,MC13TeV_QCDEM_80to120:1.26,MC13TeV_QCDEM_120to170:1.26,MC13TeV_QCDEM_170to300:1.26,MC13TeV_QCDEM_300toInf:1.26,MC13TeV_GJets_HT40to100:1.26,MC13TeV_GJets_HT100to200:1.26,MC13TeV_GJets_HT200to400:1.26,MC13TeV_GJets_HT600toInf:1.26"
-	commonOpts="-i ${outdir}/${githash}/${EXTRA} --puNormSF puwgtctr -l ${fulllumi}  --saveLog --mcUnc ${lumiUnc} --lumiSpecs VBFA:${vbflumi},OfflineVBFA:${fulllumi}"
-	#python scripts/plotter.py ${commonOpts} -j ${json} ${kFactors};
+	commonOpts="-i ${outdir}/${gh}/${EXTRA} --puNormSF puwgtctr -l ${fulllumi}  --saveLog --mcUnc ${lumiUnc} --lumiSpecs VBFA:${vbflumi},OfflineVBFA:${fulllumi}"
+	python scripts/plotter.py ${commonOpts} -j ${json} ${kFactors};
         if [[ "${EXTRA}" != *"2018"* ]]; then
-            #python scripts/plotter.py ${commonOpts}  -j data/era2017/vbf_signal_samples.json --only HighPtA_mjj -O ${outdir}/${githash}/${EXTRA}/plots_signal/ --noStack;
+            python scripts/plotter.py ${commonOpts}  -j data/era2017/vbf_signal_samples.json --only HighPtA_ -O ${outdir}/${githash}/${EXTRA}/plots_signal/ --noStack;
             python scripts/plotter.py ${commonOpts}  -j data/era2017/gjets_samples.json --only HighPtA_mjj -O ${outdir}/${githash}/${EXTRA}/plots_gjets/ --noStack;
         fi
         ;;
@@ -101,10 +108,17 @@ case $WHAT in
 	;;
 
     WWW )
-        for p in plots_gjets; do # plots plots_signal; do
-            pdir=${outdir}/${githash}/${EXTRA}/${p}
+
+        plotList=(plots plots_signal plots_gjets)
+        gh=${githash}
+        if [[ "${EXTRA}" = *"2018"* ]]; then
+            gh=${githash2018}
+        fi
+
+        for p in ${plotList[@]}; do
+            pdir=${outdir}/${gh}/${EXTRA}/${p}
             if [ -d ${pdir} ]; then
-                fdir=${wwwdir}/${githash}/${EXTRA}/${p}
+                fdir=${wwwdir}/${gh}/${EXTRA}/${p}
 	        mkdir -p ${fdir}
 	        cp ${pdir}/*.{png,pdf} ${fdir};
 	        cp test/index.php ${fdir};
