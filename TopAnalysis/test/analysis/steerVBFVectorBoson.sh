ab09259789335892033f300602dbf4d00133448d
Pedro <psilva@cern.ch>
2018-10-03 15:34:26 +0200

    adding tf script

--- TopAnalysis/test/analysis/steerVBFVectorBoson.sh
+++ TopAnalysis/test/analysis/steerVBFVectorBoson.sh
@@ -62,7 +62,7 @@ case $WHAT in
             -o ${outdir}/${githash}/${EXTRA} \
             --farmappendix ${githash} \
             -q ${queue} --genWeights genweights_${githash}.root \
-            --era era2017 -m VBFVectorBoson::RunVBFVectorBoson --ch 0 --only Data13TeV --runSysts ${extraOpts};
+            --era era2017 -m VBFVectorBoson::RunVBFVectorBoson --ch 0  --runSysts ${extraOpts};
 	;;
         #extraOpts=" --mvatree"
 
@@ -118,8 +118,10 @@ case $WHAT in
 
     PLOT )
         json=data/era2017/vbf_samples.json;
+        syst_json=data/era2017/vbf_syst_samples.json;
         lumi=${vbflumi}        
         gh=${githash}/
+        plotOutDir=${outdir}/${githash}/${EXTRA}/plots/
         if [[ "${EXTRA}" = *"2018"* ]]; then
             json=data/era2018/vbf_samples.json;
             lumi=${fulllumi2018}
@@ -127,14 +129,18 @@ case $WHAT in
             gh=${githash2018}
         fi
         kFactors="--procSF MC13TeV_QCDEM_15to20:1.26,MC13TeV_QCDEM_20to30:1.26,MC13TeV_QCDEM_30to50:1.26,MC13TeV_QCDEM_50to80:1.26,MC13TeV_QCDEM_80to120:1.26,MC13TeV_QCDEM_120to170:1.26,MC13TeV_QCDEM_170to300:1.26,MC13TeV_QCDEM_300toInf:1.26,MC13TeV_GJets_HT40to100:1.26,MC13TeV_GJets_HT100to200:1.26,MC13TeV_GJets_HT200to400:1.26,MC13TeV_GJets_HT600toInf:1.26"
-	commonOpts="-i ${outdir}/${gh}/${EXTRA} --puNormSF puwgtctr -l ${lumi}  --saveLog --mcUnc ${lumiUnc} --lumiSpecs VBFA:${vbflumi},OfflineVBFA:${fulllumi}"
-	python scripts/plotter.py ${commonOpts} -j ${json} ${kFactors} --only HighMJJ -O ${outdir}/${githash}/${EXTRA}/plots/
-	#python scripts/plotter.py ${commonOpts} -j ${json} ${kFactors} --only evcount --saveTeX --o ${outdir}/${githash}/${EXTRA}/plots/evcount_plotter.root;
+	commonOpts="-i ${outdir}/${gh}/${EXTRA} --puNormSF puwgtctr -l ${lumi}  --saveLog --mcUnc ${lumiUnc} --lumiSpecs VBFA:${vbflumi},OfflineVBFA:${fulllumi} -O ${plotOutDir}"
+	#python scripts/plotter.py ${commonOpts} -j ${json}      ${kFactors}
+	#python scripts/plotter.py ${commonOpts} -j ${syst_json} ${kFactors} --silent -o syst_plotter.root;
+
+        #transfer factors
+        python test/analysis/computeTransferFactor.py -p ${plotOutDir}/plotter.root -s ${plotOutDir}/syst_plotter.root -o ${plotOutDir};
+
         if [[ "${EXTRA}" != *"2018"* ]]; then
-            python scripts/plotter.py ${commonOpts}  -j data/era2017/vbf_signal_samples.json --only HighPtA_ -O ${outdir}/${githash}/${EXTRA}/plots_signal/ --noStack;
-            continue
+            exit -1
+            python scripts/plotter.py ${commonOpts}  -j data/era2017/vbf_signal_samples.json --only HighPtA_ -O ${outdir}/${githash}/${EXTRA}/plots_signal/ --noStack;            
             #python scripts/plotter.py ${commonOpts}  -j data/era2017/gjets_samples.json --only HighPtA_mjj -O ${outdir}/${githash}/${EXTRA}/plots_gjets/ --noStack;
-            trigOpts="-i ${outdir}/${gh}/${EXTRA} --puNormSF puwgtctr -l ${vbflumi}  --saveLog --mcUnc ${lumiUnc} ${kFactors} --only HighPtOfflineVBFA,HighPtVBFA"
+            #trigOpts="-i ${outdir}/${gh}/${EXTRA} --puNormSF puwgtctr -l ${vbflumi}  --saveLog --mcUnc ${lumiUnc} ${kFactors} --only HighPtOfflineVBFA,HighPtVBFA"
             #python scripts/plotter.py ${trigOpts} -j data/era2017/vbf_samples_2017F.json -O ${outdir}/${githash}/${EXTRA}/plots_trigger;
         fi
         ;;
