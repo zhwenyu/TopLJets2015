a525def78283beea173b912ed71059527c763357
Pedro <psilva@cern.ch>
2018-10-03 15:34:59 +0200

    adding tf script

--- TopAnalysis/test/analysis/steerVBFVectorBoson.sh
+++ TopAnalysis/test/analysis/steerVBFVectorBoson.sh
@@ -130,14 +130,13 @@ case $WHAT in
         fi
         kFactors="--procSF MC13TeV_QCDEM_15to20:1.26,MC13TeV_QCDEM_20to30:1.26,MC13TeV_QCDEM_30to50:1.26,MC13TeV_QCDEM_50to80:1.26,MC13TeV_QCDEM_80to120:1.26,MC13TeV_QCDEM_120to170:1.26,MC13TeV_QCDEM_170to300:1.26,MC13TeV_QCDEM_300toInf:1.26,MC13TeV_GJets_HT40to100:1.26,MC13TeV_GJets_HT100to200:1.26,MC13TeV_GJets_HT200to400:1.26,MC13TeV_GJets_HT600toInf:1.26"
 	commonOpts="-i ${outdir}/${gh}/${EXTRA} --puNormSF puwgtctr -l ${lumi}  --saveLog --mcUnc ${lumiUnc} --lumiSpecs VBFA:${vbflumi},OfflineVBFA:${fulllumi} -O ${plotOutDir}"
-	#python scripts/plotter.py ${commonOpts} -j ${json}      ${kFactors}
-	#python scripts/plotter.py ${commonOpts} -j ${syst_json} ${kFactors} --silent -o syst_plotter.root;
+	python scripts/plotter.py ${commonOpts} -j ${json}      ${kFactors}
+	python scripts/plotter.py ${commonOpts} -j ${syst_json} ${kFactors} --silent -o syst_plotter.root;
 
         #transfer factors
         python test/analysis/computeTransferFactor.py -p ${plotOutDir}/plotter.root -s ${plotOutDir}/syst_plotter.root -o ${plotOutDir};
 
         if [[ "${EXTRA}" != *"2018"* ]]; then
-            exit -1
             python scripts/plotter.py ${commonOpts}  -j data/era2017/vbf_signal_samples.json --only HighPtA_ -O ${outdir}/${githash}/${EXTRA}/plots_signal/ --noStack;            
             #python scripts/plotter.py ${commonOpts}  -j data/era2017/gjets_samples.json --only HighPtA_mjj -O ${outdir}/${githash}/${EXTRA}/plots_gjets/ --noStack;
             #trigOpts="-i ${outdir}/${gh}/${EXTRA} --puNormSF puwgtctr -l ${vbflumi}  --saveLog --mcUnc ${lumiUnc} ${kFactors} --only HighPtOfflineVBFA,HighPtVBFA"
