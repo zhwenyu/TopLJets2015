c0006dd15dc8ad3bc7ec27fcd42e98122b9b1e31
nadjieh <ajafari@cern.ch>
2019-03-15 09:27:09 +0100

    update

--- TopAnalysis/test/analysis/steerVBFVectorBoson.sh
+++ TopAnalysis/test/analysis/steerVBFVectorBoson.sh
@@ -94,8 +94,8 @@ case $WHAT in
         ### --CR     : gives a control region to evaluate fake rates in the photon data samples
         ### --SRfake : gives the distributions of fakes, normalised based on fake rates
 
-        #json=data/era${ERA}/vbf_samples.json,data/era${ERA}/vbf_syst_samples.json
-	json=data/era${ERA}/vbf_syst_samples.json
+        json=data/era${ERA}/vbf_samples.json,data/era${ERA}/vbf_syst_samples.json
+	#json=data/era${ERA}/vbf_syst_samples.json
 
 	if [[ -z ${EXTRA} ]]; then
 	    echo "Making trees ... "
@@ -105,7 +105,7 @@ case $WHAT in
         fi
 	echo ${json}
 	python scripts/runLocalAnalysis.py \
-	    -i ${eosdir} --only EWKAJJ \ #${json} \
+	    -i ${eosdir} --only ${json} \
             -o ${outdir}/${githash}/${EXTRA} \
             --farmappendix ${githash} \
             -q ${queue} --genWeights genweights_${githash}.root \
